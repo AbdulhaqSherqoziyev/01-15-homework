f2c2e6aec23db8389e25a67a89c2262c4d2a6dfd
Abdulhaq Sherqoziyev <abdulhaqsherqoziyev@gmail.com>
2025-01-21 14:01:47 +0500

    update

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 const express = require('express');
-const fileUpload = require('express-fileupload');
+const multer = require('multer');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-const uploadRouter = require('./routers/uploadRouter');
-const deleteRouter = require('./routers/deleteRouter');
+// Multer konfiguratsiyasi
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
 
-app.use(fileUpload());
+const upload = multer({ storage: storage });
+
+// Public papkasini statik qilib sozlash
 app.use(express.static('public'));
 
-app.use(uploadRouter);
-app.use(deleteRouter);
+// Fayl yuklash endpointi
+app.post('/upload', upload.single('file'), (req, res) => {
+    try {
+        res.send('File uploaded successfully.');
+    } catch (error) {
+        res.status(400).send('Error uploading file.');
+    }
+});
 
+// Serverni ishga tushirish
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
